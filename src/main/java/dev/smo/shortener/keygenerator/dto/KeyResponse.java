ea683fd3e38af6de17e036528f30cdb0c2fbe509
Simon Oser <simon.oser@gmail.com>
2026-02-13 15:33:53 +0100

    use record for dto/KeyResponse

--- src/main/java/dev/smo/shortener/keygenerator/dto/KeyResponse.java
+++ src/main/java/dev/smo/shortener/keygenerator/dto/KeyResponse.java
@@ -1,19 +1,6 @@
 package dev.smo.shortener.keygenerator.dto;
 
-public class KeyResponse {
-    private long id;
-    private String key;
+public record KeyResponse(long id, String key) {
 
-    public KeyResponse(long id, String key) {
-        this.id = id;
-        this.key = key;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getKey() {
-        return key;
-    }
 }
+
